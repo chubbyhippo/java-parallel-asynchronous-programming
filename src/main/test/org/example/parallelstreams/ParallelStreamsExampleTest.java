19300c6fed9f4f8bca3f30b686f741a2e78f2970
ch <mkmsacc@outlook.com>
2021-11-29 17:10:25 +0700

    Implement the rest

--- src/main/test/org/example/parallelstreams/ParallelStreamsExampleTest.java
+++ src/main/test/org/example/parallelstreams/ParallelStreamsExampleTest.java
@@ -1,10 +1,30 @@
 package org.example.parallelstreams;
 
+import org.example.util.DataSet;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.example.util.CommonUtil.startTimer;
+import static org.example.util.CommonUtil.timeTaken;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ParallelStreamsExampleTest {
 
+    ParallelStreamsExample parallelStreamsExample = new ParallelStreamsExample();
+
     @Test
     void stringTransform() {
+        List<String> inputList = DataSet.namesList();
+
+        startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform(inputList);
+        timeTaken();
+
+        assertEquals(4, resultList.size());
+        resultList.forEach(name -> {
+            assertTrue(name.contains("-"));
+        } );
     }
 }
