1c544aa31b2d0e6fb64aa94c873a50893d5cdd85
ch <mkmsacc@outlook.com>
2021-11-30 15:48:52 +0700

    Add ArrayListSpliteratorExampleTest

---
+++ src/main/test/org/example/parallelstreams/ArrayListSpliteratorExampleTest.java
@@ -0,0 +1,37 @@
+package org.example.parallelstreams;
+
+import org.example.util.DataSet;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListSpliteratorExampleTest {
+
+    ArrayListSpliteratorExample arrayListSpliteratorExample = new ArrayListSpliteratorExample();
+
+    @RepeatedTest(5)
+    void multiplyEachValue() {
+        int size = 10000000;
+        ArrayList<Integer> integers = DataSet.generateArrayList(size);
+
+        List<Integer> results = arrayListSpliteratorExample.multiplyEachValue(integers, 2, false);
+
+        assertEquals(size, results.size());
+
+    }
+
+    @RepeatedTest(5)
+    void multiplyEachValueParallel() {
+        int size = 10000000;
+        ArrayList<Integer> integers = DataSet.generateArrayList(size);
+
+        List<Integer> results = arrayListSpliteratorExample.multiplyEachValue(integers, 2, true);
+
+        assertEquals(size, results.size());
+
+    }
+}
