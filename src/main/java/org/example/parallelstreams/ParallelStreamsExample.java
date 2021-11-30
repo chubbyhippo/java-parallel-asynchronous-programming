1115a357f9ec23f5d2b2ff69963c2797d71b9a94
ch <mkmsacc@outlook.com>
2021-11-30 10:57:32 +0700

    Add stringTransform with isParallel

--- src/main/java/org/example/parallelstreams/ParallelStreamsExample.java
+++ src/main/java/org/example/parallelstreams/ParallelStreamsExample.java
@@ -19,6 +19,12 @@ public class ParallelStreamsExample {
                 .toList();
     }
 
+    public List<String> stringTransform(List<String> names, boolean isParallel) {
+
+        return isParallel ? names.parallelStream().map(this::addNameLengthTransform).toList() :
+                names.stream().map(this::addNameLengthTransform).toList();
+    }
+
     public static void main(String[] args) {
         List<String> namesList = DataSet.namesList();
         ParallelStreamsExample parallelStreamsExample = new ParallelStreamsExample();
