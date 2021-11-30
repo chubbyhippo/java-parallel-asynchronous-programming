3a32d38cc3e434e936401a93a54408cb76eca6a1
ch <mkmsacc@outlook.com>
2021-11-30 15:48:30 +0700

    Add ArrayListSpliteratorExample

---
+++ src/main/java/org/example/parallelstreams/ArrayListSpliteratorExample.java
@@ -0,0 +1,25 @@
+package org.example.parallelstreams;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.example.util.CommonUtil.*;
+
+public class ArrayListSpliteratorExample {
+    public List<Integer> multiplyEachValue(List<Integer> inputList, int multiplyValue, boolean isParallel) {
+        startTimer();
+        Stream<Integer> integerStream = inputList.stream();
+
+        if (isParallel) {
+            integerStream.parallel();
+        }
+
+        List<Integer> resultList = integerStream
+                .map(integer -> integer * multiplyValue)
+                .toList();
+
+        timeTaken();
+        stopWatchReset();
+        return resultList;
+    }
+}
