7ce354294802992df292fa2258353adc39a40c83
ch <mkmsacc@outlook.com>
2021-11-30 10:58:27 +0700

    Add stringTransform with isParallel test

--- src/main/test/org/example/parallelstreams/ParallelStreamsExampleTest.java
+++ src/main/test/org/example/parallelstreams/ParallelStreamsExampleTest.java
@@ -2,11 +2,12 @@ package org.example.parallelstreams;
 
 import org.example.util.DataSet;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.example.util.CommonUtil.startTimer;
-import static org.example.util.CommonUtil.timeTaken;
+import static org.example.util.CommonUtil.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -21,6 +22,21 @@ class ParallelStreamsExampleTest {
         startTimer();
         List<String> resultList = parallelStreamsExample.stringTransform(inputList);
         timeTaken();
+
+        assertEquals(4, resultList.size());
+        resultList.forEach(name -> assertTrue(name.contains("-")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    void stringTransformIsParallel(boolean isParallel) {
+
+        List<String> inputList = DataSet.namesList();
+
+        startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform(inputList, isParallel);
+        timeTaken();
+        stopWatchReset();
 
         assertEquals(4, resultList.size());
         resultList.forEach(name -> assertTrue(name.contains("-")));
