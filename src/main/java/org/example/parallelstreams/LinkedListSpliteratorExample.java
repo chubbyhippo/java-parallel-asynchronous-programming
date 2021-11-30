e4adbef2884e5e898400634323be98c24b0eb347
ch <mkmsacc@outlook.com>
2021-11-30 15:56:27 +0700

    Add LinkedListSpliteratorExample

--- src/main/java/org/example/parallelstreams/LinkedListSpliteratorExample.java
+++ src/main/java/org/example/parallelstreams/LinkedListSpliteratorExample.java
@@ -1,2 +1,26 @@
-package org.example.parallelstreams;public class LinkedListSpliteratorExample {
+package org.example.parallelstreams;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.example.util.CommonUtil.*;
+
+public class LinkedListSpliteratorExample {
+    public List<Integer> multiplyEachValue(LinkedList<Integer> inputList, int multiplyValue, boolean isParallel) {
+        startTimer();
+        Stream<Integer> integerStream = inputList.stream();
+
+        if (isParallel) {
+            integerStream.parallel();
+        }
+
+        List<Integer> resultList = integerStream
+                .map(integer -> integer * multiplyValue)
+                .toList();
+
+        timeTaken();
+        stopWatchReset();
+        return resultList;
+    }
 }
