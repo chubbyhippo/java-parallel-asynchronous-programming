d51441db263748b373367cbc3f2b9ef2329a9976
ch <mkmsacc@outlook.com>
2021-12-01 11:16:33 +0700

    Add CompletableFutureHelloWorld

--- src/main/java/org/example/completablefuture/CompletableFutureHelloWorld.java
+++ src/main/java/org/example/completablefuture/CompletableFutureHelloWorld.java
@@ -1,18 +1,29 @@
 package org.example.completablefuture;
 
+import lombok.RequiredArgsConstructor;
 import org.example.service.HelloWorldService;
 
 import java.util.concurrent.CompletableFuture;
 
 import static org.example.util.LoggerUtil.log;
 
+@RequiredArgsConstructor
 public class CompletableFutureHelloWorld {
+
+    private final HelloWorldService helloWorldService;
+
     public static void main(String[] args) {
-        HelloWorldService helloWorldService = new HelloWorldService();
-        CompletableFuture.supplyAsync(helloWorldService::helloWorld)
-                .thenApply(String::toUpperCase)
-                .thenAccept(s -> log("result is " + s))
-                .join();
+        CompletableFutureHelloWorld completableFutureHelloWorld =
+                new CompletableFutureHelloWorld(new HelloWorldService());
+        String result = completableFutureHelloWorld.helloWorld().join();
+        System.out.println("result = " + result);
+
         log("Done");
+    }
+
+    public CompletableFuture<String> helloWorld() {
+        return CompletableFuture.supplyAsync(helloWorldService::helloWorld)
+                .thenApply(String::toUpperCase);
+
     }
 }
