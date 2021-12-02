1245e7729c59c44eafa7bcb487c98b1284cb7999
ch <mkmsacc@outlook.com>
2021-12-02 08:13:21 +0700

    Add ProductServiceUsingCompletableFuture

---
+++ src/main/java/org/example/completablefuture/ProductServiceUsingCompletableFuture.java
@@ -0,0 +1,49 @@
+package org.example.completablefuture;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.Product;
+import org.example.domain.ProductInfo;
+import org.example.domain.Review;
+import org.example.service.ProductInfoService;
+import org.example.service.ReviewService;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.example.util.CommonUtil.stopWatch;
+import static org.example.util.LoggerUtil.log;
+
+
+@RequiredArgsConstructor
+public class ProductServiceUsingCompletableFuture {
+    private final ProductInfoService productInfoService;
+    private final ReviewService reviewService;
+
+    public Product retrieveProductDetails(String productId) {
+        stopWatch.start();
+
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture
+                .supplyAsync(()-> productInfoService.retrieveProductInfo(productId));
+        CompletableFuture<Review> reviewCompletableFuture = CompletableFuture
+                .supplyAsync(()-> reviewService.retrieveReviews(productId));
+
+        Product product = productInfoCompletableFuture
+                .thenCombine(reviewCompletableFuture, (productInfo,review)->new Product(productId, productInfo, review))
+                .join(); //block the thread
+
+        stopWatch.stop();
+        log("Total Time Taken : "+ stopWatch.getTime());
+        return product;
+    }
+
+    public static void main(String[] args) {
+
+        ProductInfoService productInfoService = new ProductInfoService();
+        ReviewService reviewService = new ReviewService();
+        ProductServiceUsingCompletableFuture productService = new ProductServiceUsingCompletableFuture(productInfoService, reviewService);
+        String productId = "ABC123";
+        Product product = productService.retrieveProductDetails(productId);
+        log("Product is " + product);
+
+    }
+}
