b0d28a57b649382d4ff52346cedb8a032d326fb2
ch <mkmsacc@outlook.com>
2021-12-03 16:16:54 +0700

    Add CompletableFutureHelloWorldExceptionExampleTest

---
+++ src/main/test/org/example/completablefuture/CompletableFutureHelloWorldExceptionExampleTest.java
@@ -0,0 +1,36 @@
+package org.example.completablefuture;
+
+import org.example.service.HelloWorldService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CompletableFutureHelloWorldExceptionExampleTest {
+
+    @Mock
+    HelloWorldService helloWorldService = mock(HelloWorldService.class);
+
+    @InjectMocks
+    CompletableFutureHelloWorldExceptionExample completableFutureHelloWorldException;
+
+
+    @Test
+    void helloWorld3AsyncCallsHandle() {
+        when(helloWorldService.hello())
+                .thenThrow(new RuntimeException("Exception Occurred"));
+        when(helloWorldService.world())
+                .thenReturn("555 abc ")
+                .thenCallRealMethod();
+
+        String result = completableFutureHelloWorldException.helloWorld3AsyncCallsHandle();
+
+        assertEquals("555 ABC  HI COMPLETABLEFUTURE!", result);
+    }
+}
