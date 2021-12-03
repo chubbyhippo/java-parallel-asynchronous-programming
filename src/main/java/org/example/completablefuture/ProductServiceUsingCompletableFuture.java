e137b57617101c74ea0c47a45d44dd0568ee2dbd
ch <mkmsacc@outlook.com>
2021-12-03 14:49:15 +0700

    Add InventoryService retrieveInventory

--- src/main/java/org/example/completablefuture/ProductServiceUsingCompletableFuture.java
+++ src/main/java/org/example/completablefuture/ProductServiceUsingCompletableFuture.java
@@ -1,29 +1,58 @@
 package org.example.completablefuture;
 
 
-import lombok.RequiredArgsConstructor;
-import org.example.domain.Product;
-import org.example.domain.ProductInfo;
-import org.example.domain.Review;
+import org.example.domain.*;
+import org.example.service.InventoryService;
 import org.example.service.ProductInfoService;
 import org.example.service.ReviewService;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static org.example.util.CommonUtil.stopWatch;
 import static org.example.util.LoggerUtil.log;
 
 
-@RequiredArgsConstructor
 public class ProductServiceUsingCompletableFuture {
     private final ProductInfoService productInfoService;
     private final ReviewService reviewService;
+    private InventoryService inventoryService;
+
+    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService,
+                                                ReviewService reviewService,
+                                                InventoryService inventoryService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+        this.inventoryService = inventoryService;
+    }
 
     public Product retrieveProductDetails(String productId) {
         stopWatch.start();
 
         CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture
                 .supplyAsync(() -> productInfoService.retrieveProductInfo(productId));
+        return getProduct(productId, productInfoCompletableFuture);
+    }
+
+    public Product retrieveProductDetailsWithInventory(String productId) {
+        stopWatch.start();
+
+        CompletableFuture<ProductInfo> productInfoCompletableFuture = CompletableFuture
+                .supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply(productInfo -> {
+                    productInfo.setProductOptions(updateInventory(productInfo));
+                    return productInfo;
+                });
+
+        return getProduct(productId, productInfoCompletableFuture);
+    }
+
+    private Product getProduct(String productId, CompletableFuture<ProductInfo> productInfoCompletableFuture) {
         CompletableFuture<Review> reviewCompletableFuture = CompletableFuture
                 .supplyAsync(() -> reviewService.retrieveReviews(productId));
 
@@ -35,6 +64,18 @@ public class ProductServiceUsingCompletableFuture {
         stopWatch.stop();
         log("Total Time Taken : " + stopWatch.getTime());
         return product;
+    }
+
+    private List<ProductOption> updateInventory(ProductInfo productInfo) {
+
+        return productInfo.getProductOptions()
+                .stream()
+                .map(productOption -> {
+                    Inventory inventory = inventoryService.retrieveInventory();
+                    productOption.setInventory(inventory);
+                    return productOption;
+                })
+                .toList();
     }
 
     public CompletableFuture<Product> retrieveCompletableFutureProduct(String productId) {
