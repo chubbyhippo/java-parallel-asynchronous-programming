be4a7d829a0b64a10c00f8046b54a69a4e380960
ch <mkmsacc@outlook.com>
2021-12-03 14:49:21 +0700

    Add InventoryService retrieveInventory test

--- src/main/test/org/example/completablefuture/ProductServiceUsingCompletableFutureTest.java
+++ src/main/test/org/example/completablefuture/ProductServiceUsingCompletableFutureTest.java
@@ -1,6 +1,7 @@
 package org.example.completablefuture;
 
 import org.example.domain.Product;
+import org.example.service.InventoryService;
 import org.example.service.ProductInfoService;
 import org.example.service.ReviewService;
 import org.junit.jupiter.api.Test;
@@ -13,8 +14,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ProductServiceUsingCompletableFutureTest {
     private final ProductInfoService productInfoService = new ProductInfoService();
     private final ReviewService reviewService = new ReviewService();
+    private InventoryService inventoryService = new InventoryService();
     private final ProductServiceUsingCompletableFuture productServiceUsingCompletableFuture =
-            new ProductServiceUsingCompletableFuture(productInfoService, reviewService);
+            new ProductServiceUsingCompletableFuture(productInfoService, reviewService, inventoryService);
+
 
     @Test
     void retrieveProductDetails() {
@@ -40,5 +43,18 @@ class ProductServiceUsingCompletableFutureTest {
             assertTrue(product.getProductInfo().getProductOptions().size() > 0);
             assertNotNull(product.getReview());
         }).join();
+    }
+
+    @Test
+    void retrieveProductDetailsWithInventory() {
+        String productId = "ABC123";
+
+        Product product = productServiceUsingCompletableFuture.retrieveProductDetailsWithInventory(productId);
+
+        assertNotNull(product);
+        assertTrue(product.getProductInfo().getProductOptions().size() > 0);
+        product.getProductInfo().getProductOptions()
+                        .forEach(productOption -> assertNotNull(productOption.getInventory()));
+        assertNotNull(product.getReview());
     }
 }
