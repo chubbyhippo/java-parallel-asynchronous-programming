322ee234d709487b76edf94dc3beb2b8b8c37db8
ch <mkmsacc@outlook.com>
2021-12-05 10:23:36 +0700

    Refactor

--- src/main/test/org/example/completablefuture/CompletableFutureHelloWorldExceptionExampleTest.java
+++ src/main/test/org/example/completablefuture/CompletableFutureHelloWorldExceptionExampleTest.java
@@ -7,15 +7,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CompletableFutureHelloWorldExceptionExampleTest {
 
     @Mock
-    HelloWorldService helloWorldService = mock(HelloWorldService.class);
+    HelloWorldService helloWorldService;
 
     @InjectMocks
     CompletableFutureHelloWorldExceptionExample completableFutureHelloWorldException;
