9f29158ff7be2cee395aa697faa0a19ad8613bd6
ch <mkmsacc@outlook.com>
2021-12-08 08:52:00 +0700

    Add exceptionally test to product service

---
+++ src/main/test/org/example/completablefuture/ProductServiceUsingCompletableFutureExceptionTest.java
@@ -0,0 +1,49 @@
+package org.example.completablefuture;
+
+import org.example.domain.Product;
+import org.example.service.InventoryService;
+import org.example.service.ProductInfoService;
+import org.example.service.ReviewService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceUsingCompletableFutureExceptionTest {
+
+    @Mock
+    private ProductInfoService productInfoService;
+    @Mock
+    private ReviewService reviewService;
+    @Mock
+    private InventoryService inventoryService;
+
+    @InjectMocks
+    ProductServiceUsingCompletableFuture productServiceUsingCompletableFuture;
+
+    @Test
+    void retrieveProductDetailsWithInventoryException() {
+        var productId = "ABC123";
+        when(productInfoService.retrieveProductInfo(any())).thenCallRealMethod();
+        when(reviewService.retrieveReviews(any()))
+                .thenThrow(new RuntimeException("Exception Occured"));
+        when(inventoryService.retrieveInventory()).thenCallRealMethod();
+
+        Product product =
+                productServiceUsingCompletableFuture.retrieveProductDetailsWithInventoryWithNonBlocking(productId);
+
+        assertNotNull(product);
+        assertTrue(product.getProductInfo().getProductOptions().size() > 0);
+        product.getProductInfo().getProductOptions()
+                .forEach(productOption -> assertNotNull(productOption.getInventory()));
+        assertNotNull(product.getReview());
+        assertEquals(0, product.getReview().getNoOfReviews());
+    }
+
+}
