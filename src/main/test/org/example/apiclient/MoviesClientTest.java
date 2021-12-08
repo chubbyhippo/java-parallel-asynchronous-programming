ccfc0b78152b140ab4a3a3fe08e0720652ab521a
ch <mkmsacc@outlook.com>
2021-12-08 15:23:24 +0700

    Add non blocking test

--- src/main/test/org/example/apiclient/MoviesClientTest.java
+++ src/main/test/org/example/apiclient/MoviesClientTest.java
@@ -1,9 +1,10 @@
 package org.example.apiclient;
 
-import org.junit.jupiter.api.Test;
+import org.example.util.CommonUtil;
+import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoviesClientTest {
     WebClient webClient = WebClient.builder()
@@ -12,14 +13,32 @@ class MoviesClientTest {
 
     MoviesClient moviesClient = new MoviesClient(webClient);
 
-    @Test
+    @RepeatedTest(10)
     void retrieveMovies() {
+        CommonUtil.startTimer();
         var movieInfoId = 1L;
 
         var movie = moviesClient.retrieveMovie(movieInfoId);
 
         System.out.println("movie : " + movie);
-        assert movie!=null;
+        CommonUtil.timeTaken();
+        CommonUtil.stopWatchReset();
+        assert movie != null;
+        assertEquals("Batman Begins", movie.getMovieInfo().getName());
+        assert movie.getReviewList().size() == 1;
+    }
+
+    @RepeatedTest(10)
+    void retrieveMoviesCompletableFuture() {
+        CommonUtil.startTimer();
+        var movieInfoId = 1L;
+
+        var movie = moviesClient.retrieveMovieCompletableFuture(movieInfoId).join();
+
+        System.out.println("movie : " + movie);
+        CommonUtil.timeTaken();
+        CommonUtil.stopWatchReset();
+        assert movie != null;
         assertEquals("Batman Begins", movie.getMovieInfo().getName());
         assert movie.getReviewList().size() == 1;
     }
