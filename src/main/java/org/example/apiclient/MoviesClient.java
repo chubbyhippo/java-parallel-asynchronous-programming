7d79638e070f4b303734c2cda40bf8f1224f10d8
ch <mkmsacc@outlook.com>
2021-12-08 15:05:01 +0700

    Add MoviesClient

---
+++ src/main/java/org/example/apiclient/MoviesClient.java
@@ -0,0 +1,47 @@
+package org.example.apiclient;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.movie.Movie;
+import org.example.domain.movie.MovieInfo;
+import org.example.domain.movie.Review;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class MoviesClient {
+    private final WebClient webClient;
+
+    public Movie retrieveMovie(Long movieInfoId) {
+        MovieInfo movieInfo = invokeMovieInfoService(movieInfoId);
+        List<Review> reviews = invokeMovieReviewsService(movieInfoId);
+        return new Movie(movieInfo, reviews);
+    }
+
+    private MovieInfo invokeMovieInfoService(Long movieInfoId) {
+        var moviesInfoUrlPath = "/v1/movie_infos/{movieInfoId}";
+        return webClient
+                .get()
+                .uri(moviesInfoUrlPath, movieInfoId)
+                .retrieve()
+                .bodyToMono(MovieInfo.class)
+                .block();
+    }
+
+    private List<Review> invokeMovieReviewsService(Long movieInfoId) {
+        String reviewUri = UriComponentsBuilder
+                .fromUriString("/v1/reviews")
+                .queryParam("movieInfoId", movieInfoId)
+                .buildAndExpand()
+                .toString();
+
+        return webClient
+                .get()
+                .uri(reviewUri)
+                .retrieve()
+                .bodyToFlux(Review.class)
+                .collectList()
+                .block();
+    }
+}
