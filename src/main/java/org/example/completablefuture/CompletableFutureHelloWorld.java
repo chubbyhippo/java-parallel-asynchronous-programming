88c02de7959350c2f394043dc1695e9ce33173f8
ch <mkmsacc@outlook.com>
2021-12-08 10:58:00 +0700

    Add defined thread pools

--- src/main/java/org/example/completablefuture/CompletableFutureHelloWorld.java
+++ src/main/java/org/example/completablefuture/CompletableFutureHelloWorld.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.service.HelloWorldService;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.example.util.CommonUtil.*;
 import static org.example.util.LoggerUtil.log;
@@ -86,6 +88,43 @@ public class CompletableFutureHelloWorld {
                 .thenCombine(world, (h, w) -> h + w) // (first,second)
                 .thenCombine(hi, (previous, current) -> previous + current)
                 .thenCombine(bye, (previous, current) -> previous + current)
+                .thenApply(String::toUpperCase)
+                .join();
+
+        timeTaken();
+
+        return hw;
+    }
+
+    public String helloWorld4AsyncCallsCustomThreadPool() {
+        startTimer();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(helloWorldService::hello, executorService);
+        CompletableFuture<String> world = CompletableFuture.supplyAsync(helloWorldService::world, executorService);
+        CompletableFuture<String> hi = CompletableFuture.supplyAsync(() -> {
+            delay(1000);
+            return " HI CompletableFuture!";
+        }, executorService);
+
+        CompletableFuture<String> bye = CompletableFuture.supplyAsync(() -> {
+            delay(1000);
+            return " Bye!";
+        });
+
+        String hw = hello
+                .thenCombine(world, (h, w) -> {
+                    log("thenCombine h/w");
+                    return h + w;
+                }) // (first,second)
+                .thenCombine(hi, (previous, current) -> {
+                    log("thenCombine grevious/current");
+                    return previous + current;
+                })
+                .thenCombine(bye, (previous, current) -> {
+                    log("thenApply");
+                    return previous + current;
+                })
                 .thenApply(String::toUpperCase)
                 .join();
 
